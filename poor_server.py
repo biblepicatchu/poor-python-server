dc0533aa753d322454bbf9eda6e7469e67390035
Korkemochka <korakarabzz@gmail.com>
2025-12-25 19:04:55 +0300

    Add /weather endpoint with query params parsing

--- poor_server.py
+++ poor_server.py
@@ -1,10 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-
+from urllib.parse import urlparse, parse_qs
 
 class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        parsed = urlparse(self.path)
+        path = parsed.path
+        query = parse_qs(parsed.query)
+
+
         print(f"[GET]{self.path}")
 
         if self.path == "/":
@@ -13,6 +18,8 @@ class MyServer(BaseHTTPRequestHandler):
             self.handle_json()
         elif self.path == "/health":
             self.handle_health()
+        elif path == "/weather":
+            self.handle_weather(query)
         else:
             self.handle_404()
 
@@ -60,6 +67,35 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(
             json.dumps(data).encode("utf-8")
         )
+    def handle_weather(self, query: dict):
+        city = query.get("city", [None])[0]
+
+        if not city:
+            self.send_response(400, "Bad Request")
+            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.end_headers()
+
+            data = {
+                "success": False,
+                "error": "Missing required query parameter: city",
+                "example": "/weather?city=Almaty"
+            }
+            self.wfile.write(json.dumps(data, ensure_ascii=False, indent=2).encode("utf-8"))
+            return
+
+        self.send_response(200, "OK")
+        self.send_header("Content-Type", "application/json; charset=utf-8")
+        self.end_headers()
+
+        data = {
+            "success":True,
+            "city": city,
+            "weather": {
+                "temp_c": 0,
+                "condition": "stub"
+            }
+        }
+        self.wfile.write(json.dumps(data, ensure_ascii=False, indent=2).encode("utf-8"))
 
     def handle_404(self):
         self.send_response(404, "Not Found")
